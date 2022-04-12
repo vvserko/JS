3ab2402b7278fed2743b33f14a3b839dc15cc282
Vladimir <89409155+vvserko@users.noreply.github.com>
2022-04-12 18:20:31 +0300

    Update main.js
    DZ26 wotking

--- DZ26_2022.04.12/main.js
+++ DZ26_2022.04.12/main.js
@@ -13,24 +13,26 @@ class Star {
 
     }
     draw(ctx) {
-        ctx.beginPath();        
-        ctx.moveTo(this.x , this.y - this.yMax);
-        let alfa = (360/this.numberOfEnds)*Math.PI/180;  
-        for(let i = 0; i < this.numberOfEnds; i++){              
-           // ctx.rotate(alfa);          
-            let x1 =  Math.sin(i*alfa) * this.yMin;
-            let y1 = this.yMax-Math.cos(i*alfa)*this.yMin;
-            ctx.lineTo(this.x + x1, this.y - this.yMax + y1);
-            ctx.lineTo(this.x + this.yMax*Math.sin(2*i*alfa), this.y - this.yMax*Math.cos(2*i*alfa));
+        ctx.beginPath();
+        ctx.translate(this.x, this.y);        
+        let alfa = (360 / this.numberOfEnds) * Math.PI / 180;
+        for (let i = 0; i < this.numberOfEnds; i++) {
+            ctx.rotate(2*i*alfa);
+            ctx.moveTo(0, - this.yMax);
+            
+            let x1 = Math.sin(alfa) * this.yMin;
+            let y1 = this.yMax - Math.cos(alfa) * this.yMin;
+            ctx.lineTo(x1, - this.yMax + y1);
+            ctx.moveTo(0, -this.yMax);
+            ctx.lineTo(-x1, - this.yMax + y1);
         }
         ctx.stroke();
-      
+
     }
 }
 const canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 800;
 const ctx = canvas.getContext('2d');
-let star1 = new Star(8, 100, 80, 'blue', 3, 'red', 400, 400);
+let star1 = new Star(16, 100, 20, 'blue', 3, 'red', 400, 400);
 star1.draw(ctx);
-
